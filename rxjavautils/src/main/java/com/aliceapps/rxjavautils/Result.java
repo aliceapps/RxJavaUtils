523937c573efe28b6048627e7dad7767c7a59501
atipisova <46962095+atipisova@users.noreply.github.com>
2020-09-08 13:23:52 +0200

    added result class

---
+++ rxjavautils/src/main/java/com/aliceapps/rxjavautils/Result.java
@@ -0,0 +1,8 @@
+package com.aliceapps.rxjavautils;
+
+public class Result {
+    public static String SUCCESS_TEXT = "AliceApps.RxJava.Success";
+    public static String ERROR_TEXT = "AliceApps.RxJava.Error";
+    public static int SUCCESS_INT = -1;
+    public static int ERROR_INT = 1;
+}

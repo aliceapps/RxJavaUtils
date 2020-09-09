800d9f6f819dda21789c9a34d841f2be2e52ebaa
atipisova <46962095+atipisova@users.noreply.github.com>
2020-09-09 21:29:09 +0200

    Added AutoDisposable class

---
+++ rxjavautils/src/main/java/com/aliceapps/rxjavautils/AutoDisposable.java
@@ -0,0 +1,34 @@
+package com.aliceapps.rxjavautils;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
+public class AutoDisposable implements LifecycleObserver {
+    private CompositeDisposable compositeDisposable;
+
+    public void bindTo(Lifecycle lifecycle) {
+        lifecycle.addObserver(this);
+        compositeDisposable = new CompositeDisposable();
+    }
+
+    public void add(Disposable disposable) throws Throwable {
+        if (compositeDisposable != null)
+            compositeDisposable.add(disposable);
+        else
+            throw new Throwable("must bind AutoDisposable to a Lifecycle first");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    public void onResume() {
+        compositeDisposable.clear();
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    public void onDestroy() {
+        compositeDisposable.dispose();
+    }
+}
